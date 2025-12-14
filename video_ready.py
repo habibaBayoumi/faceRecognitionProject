ee1b7ee7426349b84c7de64a88bdfb8cacfb6378
habiba <habibasaad362@gmail.com>
2025-12-15 00:29:11 +0200

    first commit

---
+++ video_ready.py
@@ -0,0 +1,19 @@
+
+
+import cv2
+
+cap=cv2.VideoCapture(0)
+
+while True:
+  ret,frame=cap.read()
+
+  if not ret:
+   continue
+
+  cv2.imshow('frame',frame)
+  key_pressed=cv2.waitKey(1) & 0xFF
+  if key_pressed==ord('q'):
+    break
+       
+cap.release()
+cv2.destroyAllWindows()
